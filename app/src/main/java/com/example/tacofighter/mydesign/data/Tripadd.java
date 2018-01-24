0fe3363a1c7cf4b527b4c6dd263f2dde2b8f48b7
測試一號 <abcabc>
2018-01-24 17:27:56 +0800

    TripDAO基本五項資料功能建置

--- app/src/main/java/com/example/tacofighter/mydesign/data/Tripadd.java
+++ app/src/main/java/com/example/tacofighter/mydesign/data/Tripadd.java
@@ -1,7 +1,12 @@
 package com.example.tacofighter.mydesign.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Student on 2018/1/23.
@@ -10,15 +15,67 @@ import android.database.sqlite.SQLiteDatabase;
 public class Tripadd {
     Context context;
     SQLiteDatabase db;
-    public Tripadd (Context context)
+    public Tripadd (Context context)   //新增資烙用
     {
         this.context = context;
-        TripDataBaseHelper helper = new TripDataBaseHelper(context);
+        TripaddHelper helper = new TripaddHelper(context);
         db = helper.getWritableDatabase();
     }
 
-    public boolean add(Trip t)
+    public boolean add(trip t)  //增加資料細項
     {
+        ContentValues cv = new ContentValues();
+        cv.put("_id",t.id);
+        cv.put("title",t.title);
+        cv.put("startdate",t.startdate);
+        cv.put("enddate",t.enddate);
+        cv.put("budget",t.budget);
+        return true;
+    }
 
+    public ArrayList<trip> getList(){
+        ArrayList<trip> mytrip = new ArrayList<>();
+        Cursor c = db.query("trip", new String[]{"_id","title","startdate","enddate","budget"},null,null,null,null,null);
+        if(c.moveToFirst())
+        {
+            //用於獲取指定欄位名稱的int 型別的值
+            trip t1 = new trip(c.getInt(1),c.getString(2),c.getInt(3),c.getInt(4),c.getInt(5));
+            mytrip.add(t1);
+            while (c.moveToFirst())
+            {
+                trip t = new trip(c.getInt(1),c.getString(2),c.getInt(3),c.getInt(4),c.getInt(5));
+                mytrip.add(t);
+            }
+        }
+        return mytrip;
+    }
+
+    public trip gettrip(int id)//透過ID來找 不知道能不能用其他
+    {
+        Cursor c =db.query("trip", new String[]{"_id","title","startdate","enddate","budget"},"_id=?",new String[]{String.valueOf(id)},null,null,null);
+        if(c.moveToFirst())
+        {
+            trip t1 = new trip(c.getInt(1),c.getString(2),c.getInt(3),c.getInt(4),c.getInt(5));
+            return  t1;
+        }
+        return  null;
+
+    }
+
+    public boolean update(trip t) //更新資料
+    {
+        ContentValues cv = new ContentValues();
+        cv.put("title",t.title);
+        cv.put("startdate",t.startdate);
+        cv.put("enddate",t.enddate);
+        cv.put("budget",t.budget);
+        db.update("trip",cv,"_id=?",new String[]{String.valueOf(t.id)});
+        return  true;
+    }
+
+    public boolean delete(int id)  //照ID刪除資料
+    {
+        db.delete("trip","_id=?",new String[]{String.valueOf(id)});
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/tacofighter/mydesign/data/trip.java
@@ -0,0 +1,26 @@
+package com.example.tacofighter.mydesign.data;
+
+/**
+ * Created by TacoFighter on 2018/1/21.
+ */
+
+public class trip {  //這邊放有活動中的建構式
+    public int id; //活動名
+    public String title; //人員
+    public int startdate;// 旅遊開始時間
+    public int enddate;//   旅遊結束時間
+    public int budget; //預算
+
+
+    public trip( int id, String title,int startdate,int enddate,int budget)
+    {
+        this.id = id ;
+        this.title = title ;
+        this.startdate = startdate ;
+        this.enddate=enddate ;
+        this.budget = budget ;
+
+    }
+
+
+}
